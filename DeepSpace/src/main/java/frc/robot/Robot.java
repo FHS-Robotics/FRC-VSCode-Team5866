94a80f1308f3fa276e4d7d4276217ee6c6ac9ce2
Mark Dannemiller <privatemtd@gmail.com>
2019-01-16 17:01:53 -0600

    Added simple code for printing the range output of a sensor.

--- DeepSpace/src/main/java/frc/robot/Robot.java
+++ DeepSpace/src/main/java/frc/robot/Robot.java
@@ -13,16 +13,20 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.*;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
+
   private DifferentialDrive m_myRobot_Front;
   private DifferentialDrive m_myRobot_Back;
   private Joystick m_leftStick;
   private Joystick m_rightStick;
+
+  Ultrasonic ultra = new Ultrasonic(1, 1); //Creating ultrasonic object
 
   double xSensitivity = 100; //try to stick within 0-100 range 100 being max sensitivity and 0 being no response at all
 
@@ -38,6 +42,7 @@ public class Robot extends TimedRobot {
     mainCam.setResolution(320, 240); //width and height
     CameraServer.getInstance().startAutomaticCapture(mainCam); //start camera server*/
     CameraServer.getInstance().startAutomaticCapture(); //start camera server
+    ultra.setAutomaticMode(true);
   }
 
   @Override
@@ -57,5 +62,13 @@ public class Robot extends TimedRobot {
       m_myRobot_Front.tankDrive(-m_leftStick.getY(), -m_rightStick.getY()); //all values are inverted to change which direction is front based on how the motors are wired
       m_myRobot_Back.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
     }
+
+    System.out.print(ultrasonicSample());
+  }
+
+  //returns the distance that the ultrasonic sensor is reading
+  public double ultrasonicSample()
+  {
+    return ultra.getRangeInches();
   }
 }
