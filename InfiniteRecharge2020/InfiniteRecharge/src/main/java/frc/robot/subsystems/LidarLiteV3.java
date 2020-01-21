d986e7a7c7abf24f0346125563dc0193993adcee
Your Name <ashton.neely66@frenship.net>
2020-01-20 18:14:51 -0600

    Lidar init

---
+++ InfiniteRecharge2020/InfiniteRecharge/src/main/java/frc/robot/subsystems/LidarLiteV3.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalSource;
+import edu.wpi.first.wpilibj.Counter;
+
+public class LidarLiteV3 extends SubsystemBase {
+  /**
+   * Creates a new LidarLiteV3.
+   */
+  public LidarLiteV3() {
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
