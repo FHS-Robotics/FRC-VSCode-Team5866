90673587ee276961837848b5e6776e4ad2f963fa
Your Name <ashton.neely66@frenship.net>
2020-01-22 18:30:16 -0600

    Lidar setup

--- InfiniteRecharge2020/InfiniteRecharge/src/main/java/frc/robot/subsystems/LidarLiteV3.java
+++ InfiniteRecharge2020/InfiniteRecharge/src/main/java/frc/robot/subsystems/LidarLiteV3.java
@@ -15,9 +15,27 @@ public class LidarLiteV3 extends SubsystemBase {
   /**
    * Creates a new LidarLiteV3.
    */
-  public LidarLiteV3() {
-    
-  }
+  private Counter counter;
+  private int printedWarningCount = 5;
+  private static final int calibrationOffset = 0;
+   public void LidarPWM(DigitalSource source) {
+      counter = new Counter(source);
+      counter.setMaxPeriod(1.0);
+      counter.setSemiPeriodMode(true);
+      counter.reset();
+   }
+   public double getDistance(){
+     double cm;
+     if(counter.get()<1){
+       if(printedWarningCount-- > 0){
+         System.out.println("LidarLitePWM: waiting for distance measurement");
+       }
+       return 0;
+     }
+     cm = (counter.getPeriod()*1000000.0/10.0) + calibrationOffset;
+     return cm;
+   }
+  
 
   @Override
   public void periodic() {
