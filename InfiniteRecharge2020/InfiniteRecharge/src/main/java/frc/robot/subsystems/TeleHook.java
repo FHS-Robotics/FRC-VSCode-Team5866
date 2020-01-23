7adc997fa2c12e719820d99f4960a33d6bf25d7b
Ashton Neely <57508419+Ashton-Neely@users.noreply.github.com>
2020-01-22 18:48:11 -0600

    Revert "Telescopic hook"

--- InfiniteRecharge2020/InfiniteRecharge/src/main/java/frc/robot/subsystems/TeleHook.java
+++ InfiniteRecharge2020/InfiniteRecharge/src/main/java/frc/robot/subsystems/TeleHook.java
@@ -12,32 +12,15 @@ import edu.wpi.first.wpilibj.PWMTalonSRX;
 
 public class TeleHook extends SubsystemBase {
 
-  PWMTalonSRX m_TeleCables;
-  PWMTalonSRX m_TeleHooks;
-  PWMTalonSRX  m_TeleCables;
-  PWMTalonSRX m_TeleHooks;
-  double teleState;
-  double cablePull;
+PWMTalonSRX  m_TeleCables;
+PWMTalonSRX m_TeleHooks;
+double teleState;
+double cablePull;
 
   /**
    * Creates a new TeleHook.
    */
   
-  public TeleHook(final int portUno,final int portDos) {
-    m_TeleCables = new PWMTalonSRX(portUno);
-    m_TeleHooks = new PWMTalonSRX(portDos); 
-
-  }
-
-public void extend(){
-  m_TeleHooks.set(1);
-}
-public void retract(){
-  m_TeleHooks.set(-1);
-}
-public void releaseHook(){
-  m_TeleHooks.set(0);
-}
   public TeleHook(final int portUno,final int portDos , final double state,final double pull ) {
     m_TeleCables = new PWMTalonSRX(portUno);
     m_TeleHooks = new PWMTalonSRX(portDos); 
@@ -61,9 +44,6 @@ public void up(){
 }
 public void down(){
   m_TeleCables.set(-1);
-  
-public void releaseCable(){
-  m_TeleCables.set(0);
 }
 
   @Override
