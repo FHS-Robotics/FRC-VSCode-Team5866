437c0a79bb56446a3ae4c88d329e625e7e0f3055
Jonathan <jonathan.sandjong83@frenship.net>
2022-02-08 16:14:37 -0600

    Created classes in code diagram

---
+++ 2022RapidReact/src/main/java/frc/robot/commands/BallLock.java
@@ -0,0 +1,7 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class BallLock extends CommandBase {
+    
+}
---
+++ 2022RapidReact/src/main/java/frc/robot/commands/Shoot.java
@@ -0,0 +1,7 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Shoot extends CommandBase {
+    
+}
