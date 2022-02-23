4a5d96157bd4fffdbdcc31597eda8e061423bdd1
FHS <fhsrobotics@frenship.us>
2022-02-22 18:10:02 -0600

    I removed unused `JoyStick`s from OI

--- 2022HogRider/src/main/java/frc/robot/OI.java
+++ 2022HogRider/src/main/java/frc/robot/OI.java
@@ -11,8 +11,4 @@ import edu.wpi.first.wpilibj.XboxController;
 public final class OI {
     public static XboxController driverController = new XboxController(0);
     public static XboxController gunnerController = new XboxController(1);
-
-    // TODO: Do we still need `JoyStick`s? 
-    public static Joystick driverStick = new Joystick(0);
-    public static Joystick gunnerStick = new Joystick(1);
 }
