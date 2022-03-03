089ac217713d471d42f4f77bc587e538ca7cc65b
FHS <fhsrobotics@frenship.us>
2022-03-02 19:05:59 -0600

    fixed the many errors in TeleopDrive

--- 2022HogRider/src/main/java/frc/robot/commands/TeleOpDrive.java
+++ 2022HogRider/src/main/java/frc/robot/commands/TeleOpDrive.java
@@ -6,7 +6,6 @@ import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.IntakeSystem;
 import frc.robot.utilities.Debugging;
-import frc.robot.utilities.Settings;
 import frc.robot.utilities.Debugging.Message;
 import frc.robot.OI;
 
@@ -30,53 +29,46 @@ public final class TeleOpDrive extends CommandBase {
 
       @Override
       public void execute() {
-            // GenericHID.isConnected() is very unreliable, and reports
-            // a controller as connected after unplugging it.
-
             if (OI.driverController.isConnected()) {
                   Debugging.resetSendOnce(Message.DriverUnplugged);
-                  doDriver();
+
+                  double xSpeed = -OI.driverController.getLeftY();
+                  double zRotation = OI.driverController.getRightX();
+                  m_drive.arcadeDrive(xSpeed, zRotation);
             } else {
-                  m_drive.getDrive().stopMotor();
-                  m_intakeSystem.move(0);
                   Debugging.sendOnce(Message.DriverUnplugged, "The driver's controller is unplugged!");
+
+                  m_drive.getDrive().stopMotor();
             }
             
 
             if (OI.gunnerController.isConnected()) {
                   Debugging.resetSendOnce(Message.GunnerUnplugged);
-                  doGunner();
+
+                  double armSpeed = -OI.gunnerController.getLeftY();
+                  m_arm.moveSafely(armSpeed);
+      
+                  if (OI.gunnerController.getAButton()) {
+                        m_intakeSystem.move(1);
+                  } else if (OI.gunnerController.getBButton()) {
+                        m_intakeSystem.move(-1);
+                  } else {
+                        m_intakeSystem.move(0);
+                  }
+      
+                  int pov = OI.gunnerController.getPOV();
+                  if (pov == 0) {
+                        m_elevator.move(1);
+                  } else if (pov == 180) {
+                        m_elevator.move(-1);
+                  } else {
+                        m_elevator.move(0);
+                  }
             } else {
-                  m_arm.moveSafely(0);
-                  m_elevator.move(0);
                   Debugging.sendOnce(Message.GunnerUnplugged, "The gunner's controller is unplugged!");
-            }
-      }
 
-      private void doDriver() {
-            double xSpeed = -OI.driverController.getLeftY();
-            double zRotation = OI.driverController.getRightX();
-            m_drive.arcadeDrive(xSpeed, zRotation);
-      }
-
-      private void doGunner() {
-            double armSpeed = -OI.gunnerController.getLeftY();
-            m_arm.moveSafely(armSpeed);
-
-            if (OI.gunnerController.getAButton()) {
-                  m_intakeSystem.move(Settings.INTAKE_SPEED());
-            } else if (OI.gunnerController.getBButton()) {
-                  m_intakeSystem.move(-Settings.INTAKE_SPEED());
-            } else {
                   m_intakeSystem.move(0);
-            }
-
-            int pov = OI.gunnerController.getPOV();
-            if (pov == 0) {
-                  m_elevator.move(1);
-            } else if (pov == 180) {
-                  m_elevator.move(-1);
-            } else {
+                  m_arm.moveSafely(0);
                   m_elevator.move(0);
             }
       }
