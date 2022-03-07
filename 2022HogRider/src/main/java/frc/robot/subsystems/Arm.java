1820b305ff01ace0f0da51506a5f1df30f4fe035
Jonathan <jonathan.sandjong83@frenship.net>
2022-03-07 17:06:41 -0600

    replaced SmartDashboard with Debugging in Arm

--- 2022HogRider/src/main/java/frc/robot/subsystems/Arm.java
+++ 2022HogRider/src/main/java/frc/robot/subsystems/Arm.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
+import frc.robot.utilities.Debugging;
 import frc.robot.utilities.Settings;
 
 /**
@@ -27,11 +27,14 @@ public final class Arm extends SubsystemBase {
             if (amount < .1 && amount > -.1)
                   amount = 0;
             
-            SmartDashboard.putBoolean("LIMIT", RobotMap.limitUp.get());
-            SmartDashboard.putNumber("AMM", amount);
-            if (!RobotMap.limitUp.get() && amount >= 0 && Settings.ARM_LIMITS_ENABLED())
+            Debugging.put("arm_limit_up", RobotMap.limitUp.get() ? "Yes" : "No");
+            Debugging.put("arm_current_speed", amount);
+            if (!RobotMap.limitUp.get() && amount >= 0 && Settings.ARM_LIMITS_ENABLED()) {
+                  Debugging.put("arm_brakes_on", "Yes");
                   m_arm.set(0);
-            else
+            } else {
+                  Debugging.put("arm_brakes_on", "No");
                   m_arm.set(amount);
+            }
       }
 }
