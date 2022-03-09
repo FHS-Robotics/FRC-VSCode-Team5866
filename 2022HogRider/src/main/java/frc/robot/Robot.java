d8880fb39e3405495d0af77468fd971b313b31fb
Jonathan <jonathan.sandjong83@frenship.net>
2022-03-09 09:21:17 -0600

    initialize settings on startup

--- 2022HogRider/src/main/java/frc/robot/Robot.java
+++ 2022HogRider/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.utilities.Settings;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,6 +21,7 @@ public final class Robot extends TimedRobot {
       // region general
       @Override
       public void robotInit() {
+            Settings.init();
       }
 
       @Override
--- 2022HogRider/src/main/java/frc/robot/utilities/Settings.java
+++ 2022HogRider/src/main/java/frc/robot/utilities/Settings.java
@@ -3,6 +3,10 @@ package frc.robot.utilities;
 import edu.wpi.first.wpilibj.Preferences;
 
 public class Settings {
+    public static void init() {
+        get("arm_limits_enabled", true);
+    }
+
     public static boolean get(String key, boolean defaultValue) {
         if (!Preferences.containsKey(key)) {
             Preferences.setBoolean(key, defaultValue);
