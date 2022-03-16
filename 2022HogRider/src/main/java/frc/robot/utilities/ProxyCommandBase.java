f8814877ce3c874dd5049ca35ff00e027915966a
Jonathan <jonathan.sandjong83@frenship.net>
2022-03-16 16:57:57 -0500

    Add ProxyCommandBase and test case

---
+++ 2022HogRider/src/main/java/frc/robot/utilities/ProxyCommandBase.java
@@ -0,0 +1,49 @@
+package frc.robot.utilities;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+abstract public class ProxyCommandBase extends CommandBase {
+    private Command delegate = null;
+
+    @Override
+    public void initialize() {
+        delegate = generateDelegate();
+        delegate.initialize();
+    }
+
+    @Override
+    public void execute() {
+        if (delegate == null) {
+            throw new IllegalStateException("Delegate command has already finished.");
+        }
+        delegate.execute();
+        if (delegate.isFinished()) {
+            delegate.end(false);
+            delegate = null;
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if (interrupted) {
+            if (delegate == null) {
+                throw new IllegalStateException("Delegated command must be running to interrupt.");
+            }
+            delegate.end(true);
+            delegate = null;
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return delegate == null;
+    }
+
+    /**
+     * Ran when the command is being initialized.
+     *
+     * @return the command to be substituted for.
+     */
+    abstract protected Command generateDelegate();
+}
